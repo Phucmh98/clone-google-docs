7d0438901c0ad778c4a06e94f8934ff065622ed5
Phuc <mhphuc.98@gmail.com>
2025-05-01 21:31:50 +0700

    feat[Search Input]

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -86,22 +86,7 @@ export const Editor = () => {
       Image,
       ImageResize,
     ],
-    content: `
-    <table>
-      <tbody>
-        <tr>
-          <th>Name</th>
-          <th colspan="3">Description</th>
-        </tr>
-        <tr>
-          <td>Cyndi Lauper</td>
-          <td>Singer</td>
-          <td>Songwriter</td>
-          <td>Actress</td>
-        </tr>
-      </tbody>
-    </table>
-  `,
+    content: ``,
   });
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
