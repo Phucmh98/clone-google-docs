56ffa3f4340dc9efe39af6fdca431fcc28724d0f
Phuc <mhphuc.98@gmail.com>
2025-05-02 13:43:11 +0700

    feat[Gallery Component]

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
   return (
@@ -8,7 +9,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-        Click <Link href="/documents/123">Here</Link>
+        <TemplatesGallery />
       </div>
     </div>
   );
