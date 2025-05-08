284f833541f6a2a5c2c5529c948981b07a6d2094
Phuc <mhphuc.98@gmail.com>
2025-05-08 17:11:42 +0700

    feat[Finish Collaborative margin ruler]

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -22,6 +22,7 @@ import { FontSizeExtensions } from "@/extensions/font-size";
 import { LineHeightExtensions } from "@/extensions/line-heights";
 import { types } from "util";
 import { Ruler } from "./ruler";
+import { useStorage,useMutation } from "@liveblocks/react";
 import {
   useLiveblocksExtension,
   FloatingToolbar,
@@ -29,6 +30,8 @@ import {
 import { Threads } from "./threads";
 
 export const Editor = () => {
+  const leftMargin = useStorage((root) => root.leftMargin);
+  const rightMargin = useStorage((root) => root.rightMargin);
   const liveblocks = useLiveblocksExtension();
   const { setEditor } = useEditorStore();
   const editor = useEditor({
@@ -56,7 +59,7 @@ export const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: "padding-left:56px; padding-right:56px;",
+        style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
         class:
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
@@ -65,7 +68,6 @@ export const Editor = () => {
       liveblocks,
       StarterKit.configure({
         history: false,
-
       }),
       Color,
       FontSizeExtensions,
@@ -102,8 +104,8 @@ export const Editor = () => {
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
       <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
-        <EditorContent  editor={editor} />
-        <Threads editor={editor}/>
+        <EditorContent editor={editor} />
+        <Threads editor={editor} />
       </div>
     </div>
   );
